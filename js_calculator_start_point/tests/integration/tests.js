3272cfc519953ccf1f005d9ee8f49118b34f5539
Brendan <laotsu@hotmail.co.uk>
2020-03-05 19:37:25 +0000

    Fin

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -16,4 +16,95 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it("should update display with arithmetical operations", function() {
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number2")).click();
+    element(by.css("#operator_add")).click();
+    element(by.css("#number2")).click();
+    element(by.css("#operator_equals")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal("4");
+  });
+
+  it("should update display with multi operations", function() {
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number2")).click();
+    element(by.css("#operator_add")).click();
+    element(by.css("#number2")).click();
+    element(by.css("#operator_add")).click();
+    element(by.css("#number5")).click();
+    element(by.css("#operator_subtract")).click();
+    element(by.css("#number8")).click();
+    element(by.css("#operator_equals")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal("1");
+  });
+
+  it("should display a positive output", function() {
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number2")).click();
+    element(by.css("#operator_add")).click();
+    element(by.css("#number2")).click();
+    element(by.css("#operator_add")).click();
+    element(by.css("#number5")).click();
+    element(by.css("#operator_equals")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal("9");
+  });
+
+  it("should display a negative output", function() {
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number2")).click();
+    element(by.css("#operator_subtract")).click();
+    element(by.css("#number5")).click();
+    element(by.css("#operator_equals")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal("-3");
+  });
+
+  it("should display a decimal output", function() {
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number2")).click();
+    element(by.css("#operator_divide")).click();
+    element(by.css("#number5")).click();
+    element(by.css("#operator_equals")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal("0.4");
+  });
+
+  it("should display a large output", function() {
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number9")).click();
+    element(by.css("#number9")).click();
+    element(by.css("#number9")).click();
+    element(by.css("#number9")).click();
+    element(by.css("#number9")).click();
+    element(by.css("#number9")).click();
+    element(by.css("#number9")).click();
+    element(by.css("#number9")).click();
+    element(by.css("#number9")).click();
+    element(by.css("#number9")).click();
+    element(by.css("#number9")).click();
+    element(by.css("#number9")).click();
+    element(by.css("#operator_multiply")).click();
+    element(by.css("#number5")).click();
+    element(by.css("#number5")).click();
+    element(by.css("#number5")).click();
+    element(by.css("#number5")).click();
+    element(by.css("#number5")).click();
+    element(by.css("#number5")).click();
+    element(by.css("#number5")).click();
+    element(by.css("#number5")).click();
+    element(by.css("#operator_equals")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal(
+      "55555554999944450000"
+    );
+  });
+
+  it("should display string when dividing by 0", function() {
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number6")).click();
+    element(by.css("#operator_divide")).click();
+    element(by.css("#number0")).click();
+    element(by.css("#operator_equals")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal(
+      "Can't divide by 0"
+    );
+  });
+
 });
--- js_calculator_start_point/tests/unit/calculator_spec.js
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -11,4 +11,65 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
+  it("add two numbers", function() {
+    calculator.previousTotal = "1";
+    calculator.add("4");
+    assert.equal(5.0, calculator.runningTotal);
+  });
+
+  it("subtract two numbers", function() {
+    calculator.previousTotal = "9";
+    calculator.subtract("3");
+    assert.equal(6.0, calculator.runningTotal);
+  });
+
+  it("multiply two numbers", function() {
+    calculator.previousTotal = "3";
+    calculator.multiply("3");
+    assert.equal(9.0, calculator.runningTotal);
+  });
+
+  it("divide two numbers", function() {
+    calculator.previousTotal = "2";
+    calculator.divide("2");
+    assert.equal(1.0, calculator.runningTotal);
+  });
+
+  it("number concatenate", function() {
+    calculator.numberClick("3");
+    calculator.numberClick("0");
+    assert.equal(30.0, calculator.runningTotal);
+  });
+
+  it("multi operation with equals", function() {
+    calculator.numberClick("5");
+    calculator.operatorClick("-");
+    calculator.numberClick("4");
+    calculator.operatorClick("=");
+    calculator.operatorClick("*");
+    calculator.numberClick("2");
+    calculator.operatorClick("=");
+    assert.equal(2.0, calculator.runningTotal);
+  });
+
+  it("multi operation without equals", function() {
+    calculator.numberClick("5");
+    calculator.operatorClick("-");
+    calculator.numberClick("4");
+    calculator.operatorClick("*");
+    calculator.numberClick("2");
+    calculator.operatorClick("=");
+    assert.equal(2.0, calculator.runningTotal);
+  });
+
+  it("clear running total", function() {
+    calculator.numberClick("5");
+    calculator.operatorClick("-");
+    calculator.numberClick("4");
+    calculator.operatorClick("+");
+    calculator.clearClick();
+    calculator.operatorClick("=");
+    assert.equal(1.0, calculator.runningTotal);
+  });
+
 });
